8fc58402cd1082192481328c3b294aba589bb180
LiDan.Chen@cisdi.com.cn <LiDan.Chen@cisdi.com.cn>
2020-08-21 17:13:55 +0800

    feat(only-number):输入框只能输入数字

--- src/plugins/directives/index.js
+++ src/plugins/directives/index.js
@@ -2,8 +2,14 @@
  * Created by cld on 2019/07/31.
  */
 import tip_data from './tip-data/tip_data.js'
+import loading from './loading/loading.js'
+import only_number from './input-number/only_number.js'
 
-const directives = [{ name: 'tip-data', directive: tip_data }];
+const directives = [
+    { name: 'tip-data', directive: tip_data },
+    { name: 'loading', directive: loading },
+    { name: 'only-number', directive: only_number }
+];
 
 export default {
     install(Vue) {
---
+++ src/views/OnlyNumber.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="about">
+        <el-input
+            v-model="value"
+            v-number="{ min: -8, max: 100, precision: 2 }"
+            size="small"
+        ></el-input>
+        <el-button size="small" @click="click">失去焦点按钮 </el-button>
+    </div>
+</template>
+<script>
+export default {
+    name: "OnlyNumber",
+    data() {
+        return {
+            value: -1,
+            value2: 2
+        };
+    },
+    watch: {
+        value() {
+            console.log(typeof this.value);
+        }
+    },
+    methods: {
+        click() {
+            console.log(typeof this.value === "number");
+        }
+    }
+};
+</script>
